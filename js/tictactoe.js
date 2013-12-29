c628ed8ba4fc2647401be93f39fd5297a2cafe09
Jonathan Llewellyn <inexistenz@gmail.com>
2013-12-28 23:18:13 -0500

    Encapsulating most functions into instantiable game object

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,63 +1,66 @@
-var gridMatrix = [
-    ["_","_","_"],
-    ["_","_","_"],
-    ["_","_","_"]
-];
+var tttGame = function () {
 
-var opponentMap = {
-    X : 'O',
-    O : 'X'
+    this.gridMatrix = [
+        ["_","_","_"],
+        ["_","_","_"],
+        ["_","_","_"]
+    ];
+
+    this.movesMade = 0;
+
+    this.opponentMap = {
+        X : 'O',
+        O : 'X'
+    };
 };
 
-var movesMade = 0;
-
-function setLetter(row,col,letter) {
+tttGame.prototype.setLetter = function(row,col,letter) {
     if(document.getElementById(row + '_' + col).innerHTML == '_') {
-        movesMade++;
-        console.log(letter + " r: " + row + ", c: " + col);
-        console.log("Moves made: " + movesMade);
-        document.getElementById(row + '_' + col).innerHTML = gridMatrix[row][col] = letter;
+        this.movesMade++;
+        console.log(letter + " at r: " + row + ", c: " + col);
+        console.log("Moves made: " + this.movesMade);
+        document.getElementById(row + '_' + col).innerHTML = this.gridMatrix[row][col] = letter;
         return true;
     }
     return false;
 }
 
-function checkPotentialWin(letter) {
+tttGame.prototype.checkPotentialWin = function(letter) {
     var row = null;
     var col = null;
     var count = 0;
 
-    (gridMatrix[0][0] == letter) ? count++ : (gridMatrix[0][0] == '_') ? (col = row = 0) : null/*next*/;
-    (gridMatrix[1][1] == letter) ? count++ : (gridMatrix[1][1] == '_') ? (col = row = 1) : null/*next*/;
-    (gridMatrix[2][2] == letter) ? count++ : (gridMatrix[2][2] == '_') ? (col = row = 2) : null/*next*/;
-    if(count == 2 && row != null && col != null && gridMatrix[row][col] == '_') { //return pos;
+    (this.gridMatrix[0][0] == letter) ? count++ : (this.gridMatrix[0][0] == '_') ? (col = row = 0) : null/*next*/;
+    (this.gridMatrix[1][1] == letter) ? count++ : (this.gridMatrix[1][1] == '_') ? (col = row = 1) : null/*next*/;
+    (this.gridMatrix[2][2] == letter) ? count++ : (this.gridMatrix[2][2] == '_') ? (col = row = 2) : null/*next*/;
+    if(count == 2 && row != null && col != null && this.gridMatrix[row][col] == '_') { //return pos;
         return {"row": row, "col": col};
     }
 
     count = 0; row = col = null;
-    gridMatrix[0][2] == letter ? count++ : gridMatrix[0][2] == '_' ? (row = 0, col = 2) : null/*next*/;
-    gridMatrix[1][1] == letter ? count++ : gridMatrix[1][1] == '_' ? col = row = 1 : null/*next*/;
-    gridMatrix[2][0] == letter ? count++ : gridMatrix[2][0] == '_' ? (row = 2, col = 0) : null/*next*/;
-    if(count == 2 && row != null && col != null && gridMatrix[row][col] == '_') {//return pos;
+    this.gridMatrix[0][2] == letter ? count++ : this.gridMatrix[0][2] == '_' ? (row = 0, col = 2) : null/*next*/;
+    this.gridMatrix[1][1] == letter ? count++ : this.gridMatrix[1][1] == '_' ? col = row = 1 : null/*next*/;
+    this.gridMatrix[2][0] == letter ? count++ : this.gridMatrix[2][0] == '_' ? (row = 2, col = 0) : null/*next*/;
+    if(count == 2 && row != null && col != null && this.gridMatrix[row][col] == '_') {//return pos;
         return {"row": row, "col": col};
     }
 
     count = 0; row = col = null;
-    for(row in gridMatrix) {
-        (gridMatrix[row][0] == letter) ? count++ : (gridMatrix[row][0] == '_') ? (col = 0) : null/*next*/;
-        (gridMatrix[row][1] == letter) ? count++ : (gridMatrix[row][1] == '_') ? (col = 1) : null/*next*/;
-        (gridMatrix[row][2] == letter) ? count++ : (gridMatrix[row][2] == '_') ? (col = 2) : null/*next*/;
-        if(count == 2 && row != null && col != null && gridMatrix[row][col] == '_') {//return pos;
+    for(row in this.gridMatrix) {
+        (this.gridMatrix[row][0] == letter) ? count++ : (this.gridMatrix[row][0] == '_') ? (col = 0) : null/*next*/;
+        (this.gridMatrix[row][1] == letter) ? count++ : (this.gridMatrix[row][1] == '_') ? (col = 1) : null/*next*/;
+        (this.gridMatrix[row][2] == letter) ? count++ : (this.gridMatrix[row][2] == '_') ? (col = 2) : null/*next*/;
+        if(count == 2 && row != null && col != null && this.gridMatrix[row][col] == '_') {//return pos;
             return {"row": row, "col": col};
         }
         count = 0; row = col = null;
     }
 
-    for(col in gridMatrix) {
-        (gridMatrix[0][col] == letter) ? count++ : (gridMatrix[0][col] == '_') ? (row = 0) : null/*next*/;
-        (gridMatrix[1][col] == letter) ? count++ : (gridMatrix[1][col] == '_') ? (row = 1) : null/*next*/;
-        (gridMatrix[2][col] == letter) ? count++ : (gridMatrix[2][col] == '_') ? (row = 2) : null/*next*/;
-        if(count == 2 && row != null && col != null && gridMatrix[row][col] == '_') {//return pos;
+    for(col in this.gridMatrix) {
+        (this.gridMatrix[0][col] == letter) ? count++ : (this.gridMatrix[0][col] == '_') ? (row = 0) : null/*next*/;
+        (this.gridMatrix[1][col] == letter) ? count++ : (this.gridMatrix[1][col] == '_') ? (row = 1) : null/*next*/;
+        (this.gridMatrix[2][col] == letter) ? count++ : (this.gridMatrix[2][col] == '_') ? (row = 2) : null/*next*/;
+        if(count == 2 && row != null && col != null && this.gridMatrix[row][col] == '_') {//return pos;
             return {"row": row, "col": col};
         }
         count = 0; row = col = null;
@@ -65,12 +68,12 @@ function checkPotentialWin(letter) {
     return null;
 }
 
-function getFork(letter) {
+tttGame.prototype.getFork = function(letter) {
     var firstLetter = null;
     var secondLetter = null;
-    for(row in gridMatrix){
-        for(col in gridMatrix[row]) {
-            if(gridMatrix[row][col] == letter) {
+    for(row in this.gridMatrix){
+        for(col in this.gridMatrix[row]) {
+            if(this.gridMatrix[row][col] == letter) {
                 firstLetter = {"row": row, "col": col};
                 break;
             }
@@ -82,11 +85,11 @@ function getFork(letter) {
 
     if(firstLetter == null) return null;
 
-    for(row in gridMatrix){
-        for(col in gridMatrix[row]) {
+    for(row in this.gridMatrix){
+        for(col in this.gridMatrix[row]) {
             if(row == firstLetter.row && col == firstLetter.col)
                 continue;
-            if(gridMatrix[row][col] == letter) {
+            if(this.gridMatrix[row][col] == letter) {
                 secondLetter = {"row": row, "col": col};
                 break;
             }
@@ -98,22 +101,22 @@ function getFork(letter) {
 
     if(secondLetter == null) return null;
 
-    if(gridMatrix[firstLetter.row][secondLetter.col] == '_')
+    if(this.gridMatrix[firstLetter.row][secondLetter.col] == '_')
         return {"row": firstLetter.row, "col": secondLetter.col};
-    if(gridMatrix[secondLetter.row][firstLetter.col] == '_')
+    if(this.gridMatrix[secondLetter.row][firstLetter.col] == '_')
         return {"row": secondLetter.row, "col": firstLetter.col};
 
     return null;
 }
 
-function counterFork(letter) {
-    var opponent = opponentMap[letter];
+tttGame.prototype.counterFork = function(letter) {
+    var opponent = this.opponentMap[letter];
     var firstPlay = null;
     var potentialOpponentPlay = null;
 
-    for(row in gridMatrix){
-        for(col in gridMatrix[row]) {
-            if(gridMatrix[row][col] == letter) {
+    for(row in this.gridMatrix){
+        for(col in this.gridMatrix[row]) {
+            if(this.gridMatrix[row][col] == letter) {
                 firstPlay = {"row": row, "col": col};
                 break;
             }
@@ -125,110 +128,154 @@ function counterFork(letter) {
 
     if(firstPlay == null) return null;
 
-    potentialOpponentPlay = getFork(opponent);
+    potentialOpponentPlay = this.getFork(opponent);
 
     if(potentialOpponentPlay == null) return null;
 
     // If player fork is on a side play corner
     if(Math.abs(potentialOpponentPlay.row - potentialOpponentPlay.col) == 1)
-        return playCorner();
+        return this.playCorner();
 
     // If player fork is on a corner play side
     if(Math.abs(potentialOpponentPlay.row - potentialOpponentPlay.col) == 0 ||
         Math.abs(potentialOpponentPlay.row - potentialOpponentPlay.col) == 2)
-        return playSide();
+        return this.playSide();
 
     return null;
 }
 
-function playCenter() {
-    if(movesMade != 0 && gridMatrix[1][1] == '_') {
+tttGame.prototype.playCenter = function() {
+    if(this.movesMade != 0 && this.gridMatrix[1][1] == '_') {
         return {"row": 1, "col": 1};
     }
 
     return null;
 }
 
-function playOppositeCorner(opponent) {
-    if(gridMatrix[0][0] == opponent && gridMatrix[2][2] == '_') {
+tttGame.prototype.playOppositeCorner = function(opponent) {
+    if(this.gridMatrix[0][0] == opponent && this.gridMatrix[2][2] == '_') {
         return {"row": 2, "col": 2};
     }
-    if(gridMatrix[2][2] == opponent && gridMatrix[0][0] == '_') {
+    if(this.gridMatrix[2][2] == opponent && this.gridMatrix[0][0] == '_') {
         return {"row": 0, "col": 0};
     }
-    if(gridMatrix[0][2] == opponent && gridMatrix[2][0] == '_') {
+    if(this.gridMatrix[0][2] == opponent && this.gridMatrix[2][0] == '_') {
         return {"row": 2, "col": 0};
     }
-    if(gridMatrix[2][0] == opponent && gridMatrix[0][2] == '_') {
+    if(this.gridMatrix[2][0] == opponent && this.gridMatrix[0][2] == '_') {
         return {"row": 0, "col": 2};
     }
 
     return null;
 }
 
-function playCorner() {
-    if(gridMatrix[0][0] == '_') {
+tttGame.prototype.playCorner = function() {
+    if(this.gridMatrix[0][0] == '_') {
         return {"row": 0, "col": 0};
     }
-    if(gridMatrix[2][2] == '_') {
+    if(this.gridMatrix[2][2] == '_') {
         return {"row": 2, "col": 2};
     }
-    if(gridMatrix[0][2] == '_') {
+    if(this.gridMatrix[0][2] == '_') {
         return {"row": 0, "col": 2};
     }
-    if(gridMatrix[2][0] == '_') {
+    if(this.gridMatrix[2][0] == '_') {
         return {"row": 2, "col": 0};
     }
 
     return null;
 }
 
-function playSide() {
-    if(gridMatrix[0][1] == '_') {
+tttGame.prototype.playSide = function() {
+    if(this.gridMatrix[0][1] == '_') {
         return {"row": 0, "col": 1};
     }
-    if(gridMatrix[2][1] == '_') {
+    if(this.gridMatrix[2][1] == '_') {
         return {"row": 2, "col": 1};
     }
-    if(gridMatrix[1][2] == '_') {
+    if(this.gridMatrix[1][2] == '_') {
         return {"row": 1, "col": 2};
     }
-    if(gridMatrix[1][0] == '_') {
+    if(this.gridMatrix[1][0] == '_') {
         return {"row": 1, "col": 0};
     }
 
     return null;
 }
 
-var tttAI = function(letter, order) {
+tttGame.prototype.checkWin = function(letter) {
+    if(this.gridMatrix[0][0] == letter &&
+        this.gridMatrix[0][0] == this.gridMatrix[1][1] &&
+        this.gridMatrix[1][1] == this.gridMatrix[2][2]) {
+            return true;
+        }
+
+    if(this.gridMatrix[0][2] == letter &&
+        this.gridMatrix[0][2] == this.gridMatrix[1][1] &&
+        this.gridMatrix[1][1] == this.gridMatrix[2][0]) {
+            return true;
+        }
+
+    for(row in this.gridMatrix) {
+        if(this.gridMatrix[row][0] == letter &&
+            this.gridMatrix[row][0] == this.gridMatrix[row][1] &&
+            this.gridMatrix[row][1] == this.gridMatrix[row][2]) {
+                return true;
+        }
+    }
+
+    for(col in this.gridMatrix) {
+        if(this.gridMatrix[0][col] == letter &&
+            this.gridMatrix[0][col] == this.gridMatrix[1][col] &&
+            this.gridMatrix[1][col] == this.gridMatrix[2][col]) {
+                return true;
+        }
+    }
+
+    return false;
+}
+
+tttGame.prototype.checkGameOver = function() {
+    if(this.checkWin('X'))
+        return 'X';
+
+    if(this.checkWin('O'))
+        return 'O';
+
+    if(this.movesMade == 9)
+        return 'T';
+}
+
+// AI definition
+var tttAI = function(game,letter, order) {
 
     this.letter = letter;
     this.order = order;
 
     this.funcList = [
     // 1. Check for possible win
-        function(){ return checkPotentialWin(letter);},
+        function(){ return game.checkPotentialWin(letter);},
 
     // 2. Check for block
-        function(){ return checkPotentialWin(opponentMap[letter]);},
+        function(){ return game.checkPotentialWin(game.opponentMap[letter]);},
 
     // 3. Make a fork
-        function(){ return getFork(letter);},
+        function(){ return game.getFork(letter);},
 
     // 4. Block a fork
-        function(){ return counterFork(letter);},
+        function(){ return game.counterFork(letter);},
 
     // 5. Play the Center
-        function(){ return playCenter();},
+        function(){ return game.playCenter();},
 
     // 6. Play opposite corner
-        function(){ return playOppositeCorner(opponentMap[letter]);},
+        function(){ return game.playOppositeCorner(game.opponentMap[letter]);},
 
     // 7. Play empty corner
-        function(){ return playCorner();},
+        function(){ return game.playCorner();},
 
     // 8. Play empty side
-        function(){ return playSide();}
+        function(){ return game.playSide();}
     ];
 
     this.move = function() {
@@ -237,68 +284,38 @@ var tttAI = function(letter, order) {
             pos = this.funcList[i]();
 
             if(pos != null) {
-                setLetter(pos.row,pos.col, this.letter);
+                game.setLetter(pos.row,pos.col, this.letter);
                 break;
             }
         }
     };
 }
 
-function checkWin(winner) {
-    if(gridMatrix[0][0] == winner &&
-        gridMatrix[0][0] == gridMatrix[1][1] &&
-        gridMatrix[1][1] == gridMatrix[2][2]) {
-            return true;
-        }
-
-    if(gridMatrix[0][2] == winner &&
-        gridMatrix[0][2] == gridMatrix[1][1] &&
-        gridMatrix[1][1] == gridMatrix[2][0]) {
-            return true;
-        }
-
-    for(row in gridMatrix) {
-        if(gridMatrix[row][0] == winner &&
-            gridMatrix[row][0] == gridMatrix[row][1] &&
-            gridMatrix[row][1] == gridMatrix[row][2]) {
-                return true;
-        }
-    }
-
-    for(col in gridMatrix) {
-        if(gridMatrix[0][col] == winner &&
-            gridMatrix[0][col] == gridMatrix[1][col] &&
-            gridMatrix[1][col] == gridMatrix[2][col]) {
-                return true;
-        }
-    }
-
-    return false;
-}
-
 var ai;
+var game;
 
 var setup = function() {
 
-    ai = new tttAI('O');
+    game = new tttGame();
+    ai = new tttAI(game, 'O');
 
     var str = "";
-    for (row in gridMatrix) {
-        for (col in gridMatrix[row]) {
-            str +='<span id="'+ row + '_' + col + '">'+gridMatrix[row][col]+'</span> ';
+    for (row in game.gridMatrix) {
+        for (col in game.gridMatrix[row]) {
+            str +='<span id="'+ row + '_' + col + '">'+game.gridMatrix[row][col]+'</span> ';
         }
         str += "<br />"
     }
 
     document.getElementById('main').innerHTML = str;
 
-    for (row in gridMatrix) {
-        for (col in gridMatrix[row]) {
+    for (row in game.gridMatrix) {
+        for (col in game.gridMatrix[row]) {
             document.getElementById(row + '_' + col).addEventListener('click', (function(r,c) {
                 return function() {
-                    var set = setLetter.call(this,r,c,'X');
+                    var set = game.setLetter(r,c,'X');
                     if(set) {
-                        if(checkWin('X')) {
+                        if(game.checkWin('X')) {
                             alert("X wins!");
                             console.log("alert clicked!");
                         } else {
